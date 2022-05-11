4d6623e7a3fcb7c4e94e29165718636fa368ec11
Elena P <elena.pan.ep@gmail.com>
2022-05-11 15:49:15 -0400

    Connect front and backend

---
+++ client/build/precache-manifest.026bde5dcf780c719d3c516ef9dabbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96854f00e77d4c4549bda87ca41e1485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a786a0ab6c34065ff6",
+    "url": "/static/css/main.0dc05347.chunk.css"
+  },
+  {
+    "revision": "e991997b51fe6a58a18b",
+    "url": "/static/js/2.ea15b47a.chunk.js"
+  },
+  {
+    "revision": "a161c3052ad3cbb94a04737a4abc4b82",
+    "url": "/static/js/2.ea15b47a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a786a0ab6c34065ff6",
+    "url": "/static/js/main.99d81220.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
